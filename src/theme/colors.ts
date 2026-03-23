acfbbe3c8673c1d1c227104d8b094d5073b3bcc2
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-23 13:16:19 -0600

    Create colors.ts

---
+++ src/theme/colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  primary:   '#00A651',   // verde pinolero
+  danger:    '#E30613',   // rojo logo
+  dark:      '#1a1a1a',
+  light:     '#f5f5f5',
+  gray:      '#6b7280',
+  grayLight: '#d1d5db',
+}

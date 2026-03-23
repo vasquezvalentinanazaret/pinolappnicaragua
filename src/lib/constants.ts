1e003a885326f8848454304b6493c968222a9ca8
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-23 13:16:58 -0600

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const CURRENCY = 'C$'
+export const APP_NAME = 'PinolApp Nicaragua'
+export const DELIVERY_FEE = 50
+
+// Temporal – luego vendrá de Supabase o API
+export const MOCK_RESTAURANTS = [
+  { id: '1', name: 'El Buen Sabor', rating: 4.7, time: '15-25 min', category: 'Típico' },
+  { id: '2', name: 'Súper Típico',  rating: 4.5, time: '20-30 min', category: 'Comida rápida' },
+  { id: '3', name: 'La Fritanga',    rating: 4.8, time: '10-20 min', category: 'Fritanga' },
+]

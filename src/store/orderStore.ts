e45eb88399c6be43bdf11a5ef38a2f1a9f5a6a1b
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-23 13:46:35 -0600

    Create orderStore.ts

---
+++ src/store/orderStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+import { Order } from '@/types';
+
+interface OrderState {
+  orders: Order[];
+  addOrder: (order: Order) => void;
+  updateOrderStatus: (id: string, status: Order['status']) => void;
+}
+
+export const useOrderStore = create<OrderState>((set) => ({
+  orders: [],
+  addOrder: (order) => set((state) => ({ orders: [...state.orders, order] })),
+  updateOrderStatus: (id, status) =>
+    set((state) => ({
+      orders: state.orders.map((o) => (o.id === id ? { ...o, status } : o)),
+    })),
+}));

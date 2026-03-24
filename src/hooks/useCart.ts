fa8988fddc140fbd665f459b445f52f5ebf8667e
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-24 09:17:33 -0600

    Update useCart.ts

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -3,3 +3,19 @@ import { useCartStore } from "@/src/store/cartStore";
 export const useCart = () => {
   const items = useCartStore((state) => state.items);
   const addItem = useCartStore((state) => state.addItem);
+  const removeItem = useCartStore((state) => state.removeItem);
+  const updateQuantity = useCartStore((state) => state.updateQuantity);
+  const clearCart = useCartStore((state) => state.clearCart);
+  const getTotalPrice = useCartStore((state) => state.getTotalPrice);
+  const getTotalQuantity = useCartStore((state) => state.getTotalQuantity);
+
+  return {
+    items,
+    addItem,
+    removeItem,
+    updateQuantity,
+    clearCart,
+    getTotalPrice,
+    getTotalQuantity,
+  };
+};

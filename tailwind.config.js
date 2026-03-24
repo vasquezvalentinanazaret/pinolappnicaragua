d7f7183ef7b5638d97f562a1a0e3673fcb1ecc3c
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-24 08:41:56 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        pinolGreen: "#00A651",
-        pinolRed: "#E30613",
+        primary: "#00A651",
+        secondary: "#E30613",
+        background: "#F5F5F5",
+        text: "#333333",
+      },
+      fontFamily: {
+        sans: ["System"],
       },
     },
   },
   plugins: [],
-}
+};

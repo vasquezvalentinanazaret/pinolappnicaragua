43e084689c330ce4620f64145159ee59ca78a1c8
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-24 09:02:44 -0600

    Create tailwind.config.js

---
+++ src/theme/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00A651",
+        secondary: "#E30613",
+        background: "#F5F5F5",
+        text: "#333333",
+      },
+    },
+  },
+  plugins: [],
+};

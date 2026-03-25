4dcdef3c20da84d3ece82ac17a628e63e71137c6
vasquezvalentinanazaret <vasquezvalentinanazareth@gmail.com>
2026-03-24 23:25:27 -0600

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,3 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL || "http://localhost:3000/api";
-export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL || "";
-export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "";
-
 export const DELIVERY_FEE = 30;
 export const MIN_ORDER_AMOUNT = 100;
+export const APP_NAME = "PinolApp";
